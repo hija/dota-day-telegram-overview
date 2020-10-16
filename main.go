8c781d30650d96a025173ebb0f70655a0b826635
hija <hilko.j@wwu.de>
2020-10-16 22:09:03 +0200

    Added team.json

--- main.go
+++ main.go
@@ -3,20 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	recentMatches := GetRecentMatches(64989974)
-	for _, recentMatch := range recentMatches {
+	team := GetTeam()
+	var matches int64[]
 
-		// Filter out games which were not all pick
-		if recentMatch.GameMode != 1 && recentMatch.GameMode != 22 {
-			continue
-		}
-
-		fmt.Print(recentMatch.MatchID)
-
-		if recentMatch.PlayerSlot <= 127 && recentMatch.RadiantWin {
-			fmt.Println(" --> Gewonnen!")
-		} else {
-			fmt.Println(" --> Verloren :(")
-		}
+	for _, teammember := range team.Members {
+		fmt.Printf("Lade Daten für %s (Id: %d) \r\n", teammember.Name, teammember.Id)
 	}
+
+	// recentMatches := GetRecentMatches(64989974)
+	// for _, recentMatch := range recentMatches {
+
+	// 	// Filter out games which were not all pick
+	// 	if recentMatch.GameMode != 1 && recentMatch.GameMode != 22 {
+	// 		continue
+	// 	}
+
+	// 	fmt.Print(recentMatch.MatchID)
+
+	// 	if recentMatch.PlayerSlot <= 127 && recentMatch.RadiantWin {
+	// 		fmt.Println(" --> Gewonnen!")
+	// 	} else {
+	// 		fmt.Println(" --> Verloren :(")
+	// 	}
+	// }
 }
---
+++ team.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type TeamMember struct {
+	Name string `json:"name"`
+	Id   int32  `json:"userid"`
+}
+
+type Team struct {
+	Members []TeamMember `json:"members"`
+}
+
+func GetTeam() Team {
+	// Datei einlesen und parsen
+
+	// Datei einlesen...
+	jsonFile, err := os.Open("team.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer jsonFile.Close()
+
+	// ... und parsen
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	// we initialize our Users array
+	var team Team
+
+	// we unmarshal our byteArray which contains our
+	// jsonFile's content into 'users' which we defined above
+	json.Unmarshal(byteValue, &team)
+
+	return team
+}
